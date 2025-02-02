d8251706cf6f6e8f9d0afec2ac0202a999793e1b
maus <orca@mac1.local>
2025-02-02 10:08:59 -0500

    mssa test data

---
+++ src/abx_transformation/main.py
@@ -0,0 +1,22 @@
+from ..util import excel_to_df
+import argparse
+import pandas as pd
+
+
+def main(args):
+    final_result_dates = excel_to_df(args.f)
+    print(final_result_dates.head())
+    mssa_dot = excel_to_df(args.g)
+    print(mssa_dot.head())
+
+    mssa_dot.groupby(["PAT_ENC_CSN_ID", "ABX_Category"])
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='transform data')
+    parser.add_argument('--f', type=str, required=True, help='path to file with final result dates')
+    parser.add_argument('--g', type=str, required=True, help='path to file with mssa dot data')
+
+    args = parser.parse_args()
+
+    main(args)
--- src/util.py
+++ src/util.py
@@ -1,7 +1,14 @@
 import pandas as pd
+import json
 
 
 def excel_to_df(path: str) -> pd.DataFrame:
     file = pd.ExcelFile(path)
     df = pd.read_excel(file)
     return df
+
+
+def read_json(path: str) -> None:
+    with open(path, 'r') as f:
+        data = json.load(f)
+    return data
