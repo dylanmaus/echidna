6e411d365fd0a0bf542913746315992085181f25
dylanmaus <dylan.p.maus@gmail.com>
2025-02-11 00:06:52 -0500

    updates

--- abx_transformation.py
+++ abx_transformation.py
@@ -25,44 +25,67 @@ def unstack_abx(mssa_dot: pd.DataFrame, first_or_last: str) -> pd.DataFrame:
 
 
 def main(args):
-    # final_result_dates = excel_to_df(args.f)
+    # read in files to DataFrames
+    final_result_dates = excel_to_df(args.f)
     mssa_dot = excel_to_df(args.g)
+    print(mssa_dot.head(final_result_dates.shape[0]))
+    print(mssa_dot.head(mssa_dot.shape[0]))
+
+    # date columns
+    final_result_dates["Final_Result_Date"] = pd.to_datetime(final_result_dates["Final_Result_Date"])
     mssa_dot["First_Admin"] = pd.to_datetime(mssa_dot["First_Admin"])
+    mssa_dot["Last_Admin"] = pd.to_datetime(mssa_dot["Last_Admin"])
+
+    # remove times for easier readability
     mssa_dot["First_Admin"] = mssa_dot["First_Admin"].dt.date
     mssa_dot["First_Admin"] = pd.to_datetime(mssa_dot["First_Admin"])
-    mssa_dot["Last_Admin"] = pd.to_datetime(mssa_dot["Last_Admin"])
     mssa_dot["Last_Admin"] = mssa_dot["Last_Admin"].dt.date
     mssa_dot["Last_Admin"] = pd.to_datetime(mssa_dot["Last_Admin"])
-    print(mssa_dot.head(14))
 
-    mssa_dot["delta"] = (
+    # keep earliest date per CSN in final result dates
+    earliest_final_result_dates = final_result_dates.loc[final_result_dates.groupby("CSN")["Final_Result_Date"].idxmin()]
+
+    # remove admins that entirely occur before the final result date
+    # mssa_dot = mssa_dot[mssa_dot["Last_Admin"] >= ]
+
+    # calculate deltas between rows to find boundaries between courses
+    mssa_dot["prev_delta"] = (
         mssa_dot.groupby(["PAT_ENC_CSN_ID", "ABX_Category"], as_index=False)
         .apply(lambda x: x["First_Admin"].dt.day - x["Last_Admin"].dt.day.shift(1), include_groups=False)
         .reset_index(drop=True)
     )
-    mssa_dot.replace({"delta": {1: 0}}, inplace=True)
-    mssa_dot.fillna({"delta": 1}, inplace=True)
-    mssa_dot.loc[mssa_dot["delta"] > 1, "delta"] = 1
+
+    # assign each admin to a course
+    mssa_dot.replace({"prev_delta": {1: 0}}, inplace=True)
+    mssa_dot.fillna({"prev_delta": 1}, inplace=True)
+    mssa_dot.loc[mssa_dot["prev_delta"] > 1, "prev_delta"] = 1
     mssa_dot.replace(0, np.nan, inplace=True)
-
-    mssa_dot["course"] = mssa_dot.groupby(["PAT_ENC_CSN_ID", "ABX_Category", "delta"]).cumcount()
-
+    mssa_dot["course"] = mssa_dot.groupby(["PAT_ENC_CSN_ID", "ABX_Category", "prev_delta"]).cumcount()
     mssa_dot.ffill(inplace=True)
-
     mssa_dot["course"] += 1
     mssa_dot["course"] = mssa_dot["course"].astype(int)
+
+    # label each abx course
     mssa_dot["category"] = mssa_dot["ABX_Category"] + "_" + mssa_dot["course"].astype(str)
 
-    # print(mssa_dot.head(15))
 
+    mssa_dot["admin_delta"] = mssa_dot["Last_Admin"] - mssa_dot["First_Admin"]
+    mssa_dot.replace({"admin_delta": {pd.Timedelta("0 days"): pd.Timedelta("1 days")}}, inplace=True)
+
+
+    # mssa_dot.groupby(["PAT_ENC_CSN_ID", "ABX_Category", "course"])("admin_delta").sum()
+
+    print(mssa_dot.head(15))
+
+    # display each CSNs data on a single row
     first_admin = unstack_abx(mssa_dot=mssa_dot, first_or_last="First_Admin")
     last_admin = unstack_abx(mssa_dot=mssa_dot, first_or_last="Last_Admin")
-
     result = pd.merge(first_admin, last_admin)
     result = result[[result.columns[0]] + sorted(result.columns[1:])]
 
     print(result.head(15))
 
+    # create excel file
     result.to_excel("output.xlsx", index=False)
 
 
