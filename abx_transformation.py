df9b1b8c03bf86297fcda82c734e55b89281d636
dylanmaus <dylan.p.maus@gmail.com>
2025-02-12 23:26:27 -0500

    create graph to find course start and end times

--- abx_transformation.py
+++ abx_transformation.py
@@ -1,6 +1,111 @@
 import argparse
 import pandas as pd
 import numpy as np
+from datetime import timedelta
+
+
+class Graph:
+    """Create graph in adjacency list form"""
+
+    def __init__(self):
+        self.nodes: dict = {}
+
+    def add_node(self, name: str) -> None:
+        if name not in self.nodes:
+            self.nodes[name] = []
+
+    def add_neighbor(self, name: str, neighbor: str) -> None:
+        if neighbor not in self.nodes[name]:
+            self.nodes[name].append(neighbor)
+
+
+def build_graph(data: list[tuple]) -> Graph:
+    graph = Graph()
+
+    for s, d in data:
+        graph.add_node(s)
+        graph.add_node(d)
+        graph.add_neighbor(s, d)
+        graph.add_neighbor(d, s)
+
+    return graph
+
+
+def dfs_iterative(graph: Graph) -> list:
+    explored = []
+    components = []
+
+    for node in list(graph.nodes):
+        if node in explored:
+            continue
+
+        stack = []
+        stack.append(node)
+
+        subgraph = []
+
+        while True:
+            if len(stack) == 0:
+                break
+
+            next_node = stack.pop()
+
+            if next_node in explored:
+                continue
+
+            subgraph.append(next_node)
+            explored.append(next_node)
+
+            for neighbor in graph.nodes[next_node]:
+                if neighbor in explored:
+                    continue
+                stack.append(neighbor)
+
+        components.append(subgraph)
+
+    return components
+
+
+def expand_dates(df: pd.DataFrame):
+    start_dates = df["First_Admin"].dt.date.tolist()
+    end_dates = df["Last_Admin"].dt.date.tolist()
+    pairs = list(zip(start_dates, end_dates))
+
+    # add connecting date pairs
+    for date in start_dates:
+        next_day = date + timedelta(days=1)
+        if next_day in end_dates:
+            pairs.append((date, next_day))
+        previous_day = date - timedelta(days=1)
+        if previous_day in end_dates:
+            pairs.append((previous_day, date))
+
+    expanded = []
+    for start_date, end_date in pairs:
+        if start_date == end_date:
+            expanded.append((start_date, end_date))
+        else:
+            current_date = start_date
+            while True:
+                if current_date == end_date:
+                    break
+                next_date = current_date + timedelta(days=1)
+                expanded.append((current_date, next_date))
+                current_date = next_date
+
+    # remove duplicates
+    return list(set(expanded))
+
+
+def assign_courses(df: pd.DataFrame):
+    expanded_dates = expand_dates(df=df)
+
+    for date in expanded_dates:
+        print(date)
+    graph = build_graph(data=expanded_dates)
+    components = dfs_iterative(graph=graph)
+
+    return components
 
 
 def excel_to_df(path: str) -> pd.DataFrame:
@@ -28,8 +133,7 @@ def main(args):
     # read in files to DataFrames
     final_result_dates = excel_to_df(args.f)
     mssa_dot = excel_to_df(args.g)
-    print(mssa_dot.head(final_result_dates.shape[0]))
-    print(mssa_dot.head(mssa_dot.shape[0]))
+    # print(mssa_dot.head(final_result_dates.shape[0]))
 
     # date columns
     final_result_dates["Final_Result_Date"] = pd.to_datetime(final_result_dates["Final_Result_Date"])
@@ -52,6 +156,9 @@ def main(args):
     # remove admins that entirely occur before the final result date
     mssa_dot = mssa_dot[mssa_dot["Last_Admin"] >= mssa_dot["Final_Result_Date"]]
     mssa_dot.reset_index(inplace=True, drop=True)
+    print(mssa_dot.head(mssa_dot.shape[0]))
+
+    assign_courses(mssa_dot)
 
     # calculate deltas between rows for each abx group to find boundaries between abx courses
     mssa_dot.sort_values(by=["PAT_ENC_CSN_ID", "ABX_Category", "First_Admin"], ascending=True, inplace=True)
